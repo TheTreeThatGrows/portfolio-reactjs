b7ebe75c781b20b506c22873cd1d05130794ad48
TheTreeThatGrows <ejlaguardia@hotmail.com>
2022-08-10 01:08:53 +0200

    Heroku fix

--- server.js
+++ server.js
@@ -15,3 +15,14 @@ app.use("/", contactRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log('server listening to port 5000'));
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
